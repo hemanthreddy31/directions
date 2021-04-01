0b244cab6aa0acbe57995d4ff4f9b2e321b47b1b
FS2315 <81744733+FS2315@users.noreply.github.com>
2021-04-01 14:29:17 +0530

    directions

---
+++ showdirections.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+  string e,w,n,s;
+  e="east";
+  w="west";
+  n="north";
+  s="south";
+  printf("%s",e);
+    printf("%s",w);
+    printf("%s",n);
+    printf("%s",s);
+  return 0;
+}
